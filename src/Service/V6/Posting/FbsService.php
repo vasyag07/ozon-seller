e6bfeda6b29fd627c57bb9346711de85e4fa83ac
Vasiagin Sergei <vasyagin@intaro.email>
2025-12-23 13:15:35 +0300

    Учтено, что метод v6/fbs/posting/product/exemplar/set возвращает теперь не булевый результат, а объект

--- src/Service/V6/Posting/FbsService.php
+++ src/Service/V6/Posting/FbsService.php
@@ -28,7 +28,7 @@ class FbsService extends AbstractService
      *
      * @param array $exemplarData
      */
-    public function setExemplar($exemplarData): bool
+    public function setExemplar($exemplarData): array
     {
         $exemplarData = ArrayHelper::pick($exemplarData, ['posting_number', 'products']);
 
